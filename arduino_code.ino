abfc1ce64e891b659d47bf96c0cd99cd2aeda89f
mxArkel86 <nicholas.lkading@gmail.com>
2021-11-17 13:34:50 -0800

    code

--- arduino_code.ino
+++ arduino_code.ino
@@ -9,19 +9,41 @@ int kP = 0;           // proportional gain coeficient
 int kI = 0;           // integral gain coeficient
 int kD = 0;           // derivative gain coeficient
 
+#define BLACK 0
+#define WHITE 1023
+
 //PHOTORESISTOR DEF
 #define totalPhotoResistors 7 // Defining how many photoresistors are used, modify this if more or less are used
 
+//DECLARE FUNCTIONS
+void InitWeights(int);
+
 struct Weight{
-  int while_val = 1023;
-  int black_val = 0;
-}
+  int while_val = WHITE;
+  int black_val = BLACK;
+};
 // Initialize Photo Resistor Array
 int PRVals[totalPhotoResistors]; // Since there are 7 photoresistors, we will number them from 0 to 6 (recall array index start with 0)
 Weight PRWeights[totalPhotoResistors];
 
+void SmartDelay(int n, int m=1){// {MS}, {TIME INCREMENT (0.5)}
+  for(float i =0;i<n;i+=m){
+    delay((int)(m*1000));
+    Serial.print("t=");
+    Serial.println(n-i);
+  }
+}
+
 void setup() { /* Setup - runs once (when power is supplied or after reset) */
   Serial.begin(9600); // For serial communication set up
+  
+  Serial.println("INIT WHITE");
+  SmartDelay(3000, 0.5);
+  InitWeights(WHITE);
+  
+  Serial.println("INIT BLACK");
+  SmartDelay(3000, 0.5);
+  InitWeights(BLACK);
 }
 
 void ReadPhotoResistors() {
@@ -34,13 +56,35 @@ void ReadPhotoResistors() {
   }
 }
 
+int MapPhotoResistor(int val, int min_resolution, int max_resolution, int min_potentiometer, int max_potentiometer) {
+  return map(val, min_resolution, max_resolution, min_potentiometer, max_potentiometer);
+}
+
+void InitWeights(int type){
+  ReadPhotoResistors();
+  if(type==WHITE){//white
+    for(int i =0;i<totalPhotoResistors;i++){
+      PRWeights[i].while_val = PRVals;
+    }
+  }else if(type==BLACK){//black
+    for(int i =0;i<totalPhotoResistors;i++){
+      PRWeights[i].black_val = PRVals;
+    }
+  }
+}
+
+
 void NormalizePhotoResistorValues(){
   for(int i =0;i<totalPhotoResistors;i++){
     Weight w = PRWeights[i];
     int val = PRVals[i];
-    int outval = MapPotentiometer(val, w.while_val, w.black_val, 0, 1023);
+    int outval = MapPhotoResistor(val, w.while_val, w.black_val, 0, 1023);
     PRVals[i] = outval;
   }
+}
+
+void ReadPotentiometers(){
+
 }
 
 void loop() { /* Loop - loops forever (until unpowered or reset) */
@@ -58,10 +102,7 @@ void loop() { /* Loop - loops forever (until unpowered or reset) */
 }
 
 // ************************************************************************************************* //
-// function to read and map values from potentiometers
-int MapPhotoResistor(int val, int min_resolution, int max_resolution, int min_potentiometer, int max_potentiometer) {
-  return map(val, min_resolution, max_resolution, min_potentiometer, max_potentiometer);
-}
+
 
 // ************************************************************************************************* //
 // function to print values of interest
